798e8175c5db457797a00d02696a9f8079610cfd
ProskurinVadim <proskurin.vadim@gmail.com>
2023-07-27 13:48:23 +0100

    add fixes, connect all except items

--- components/views/shared/OurTeam/getData.tsx
+++ components/views/shared/OurTeam/getData.tsx
@@ -3,7 +3,7 @@ import TestImage from "../../../../public/people-test.jpg";
 
 export const getData = (data: any[]) => data.map(({ attributes }: any) => ({
 	person: {
-		image: attributes.Photo ? attributes.Photo.data.attributes.url : TestImage,
+		image: attributes.Photo && attributes.Photo.data ? attributes.Photo.data.attributes.url : TestImage,
 		name: attributes.FullName,
 		job: attributes.Category,
 	},
