9add350d05df82927d92977ad068c8b222ab63e5
unknown <bogdan.lyamzin.d@gmail.com>
2023-07-28 10:53:57 +0300

    fix mobile width

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    images: {
+        domains: ['www.res.cloudinary.com'],
+    },
     experimental: {
         serverActions: true,
     },
