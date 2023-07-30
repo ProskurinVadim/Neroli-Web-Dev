fec665f0b4421923ba76982f9c6a3779d2bb49f3
unknown <bogdan.lyamzin.d@gmail.com>
2023-07-30 16:24:37 +0300

    add revalidate

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,6 +3,8 @@ import { Comments, OurTeam } from "../../components/views/shared/";
 import { getBestAggents, getAboutUs } from "../../utils/fetch";
 import { formatAgentsData } from "../../utils/formater";
 
+export const revalidate = 60;
+
 const AboutUsPage = async () => {
     const contentData = getAboutUs();
     const agentsData =  getBestAggents();
