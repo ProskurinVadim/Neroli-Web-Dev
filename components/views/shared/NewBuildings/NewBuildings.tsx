eab30b24123b5d4dd2a6a92c96814cd0fb9a33b1
unknown <bogdan.lyamzin.d@gmail.com>
2023-08-01 06:55:02 +0300

    fix

--- components/views/shared/NewBuildings/NewBuildings.tsx
+++ components/views/shared/NewBuildings/NewBuildings.tsx
@@ -5,6 +5,7 @@ import Container from "../../../../hoc/Container";
 import Toggler from "../../../common/Toggler";  
 import Carousel from "../../../shared/Carousel";
 import Card from "../../../shared/Card/AppartmentCard";
+import SwiperNewBuildingCarousel from "./SwiperNewBuildingCarousel/SwiperNewBuildingCarousel";
 import { getCarouselData } from "./getData";
 import useItems from "../../../../hooks/useItems";
 import styles from "./NewBuildings.module.scss";
@@ -37,13 +38,17 @@ const NewBuildings = () => {
             <Container>
                 <h2 className="section_header">New this week</h2>
                 <Toggler array={["For sale", "Off-Plan"]} active={active} setActive={handelSetActive} className={styles.toggler} />
-                <Carousel config={config} data={filteredItems}
-                    Item={(props: any) =>
-                        <Link className="link_unset" href={`/list/${props.id}`}>
-                            <Card {...props} className={styles.card} />
-                        </Link>
-                    }
-                />
+                <div className={styles.sliderContainer}>
+                    <SwiperNewBuildingCarousel data={filteredItems} />
+                </div>
+
+                {/*<Carousel config={config} data={filteredItems}*/}
+                {/*    Item={(props: any) =>*/}
+                {/*        <Link className="link_unset" href={`/list/${props.id}`}>*/}
+                {/*            <Card {...props} className={styles.card} />*/}
+                {/*        </Link>*/}
+                {/*    }*/}
+                {/*/>*/}
             </Container>
         </section>
     )
--- components/views/shared/NewBuildings/SwiperNewBuildingCarousel/SwiperNewBuildingCarousel.jsx
+++ components/views/shared/NewBuildings/SwiperNewBuildingCarousel/SwiperNewBuildingCarousel.jsx
@@ -1,49 +1,49 @@
 import { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Card from "../../../../shared/Card/AppartmentCard";
+import Link from 'next/link';
 import {nanoid} from "nanoid";
 import Person, { personStyles } from "../../../../shared/Person/Person";
+import styles from "../NewBuildings.module.scss";
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-import './styles.css';
+import './styles.scss';
 
 // import required modules
 import { Pagination } from 'swiper/modules';
 import { Navigation } from 'swiper/modules';
 
-export default function SwiperCommentsCarousel({data}) {
 
-    const elements = data.map(item => <SwiperSlide key={nanoid()}><Person className={personStyles.person_comments}  {...item} /></SwiperSlide>)
+export default function SwiperNewBuildingCarousel({data}) {
+
+    const elements = data.map(item => <SwiperSlide key={nanoid()}> <Link className="link_unset" href={`/list/${item.id}`}><Card {...item} className={styles.card} /></Link></SwiperSlide>)
     return (
         <>
             <Swiper
                 // slidesPerView={2.5}
                 // spaceBetween={40}
                 // centeredSlides={true}
-                // navigation={true}
+                navigation={true}
                 pagination={{
                     clickable: true,
                 }}
                 breakpoints={{
-                    440: {
-                        slidesPerView: 1,
-                        spaceBetween: 0,
-                    },
-                    768: {
+                    320: {
                         slidesPerView: 2,
-                        spaceBetween: 30,
+                        spaceBetween: 20,
                     },
                     1024: {
-                        slidesPerView: 2.5,
+                        slidesPerView: 3,
                         spaceBetween: 40,
                     },
                 }}
                 modules={[Navigation, Pagination]}
-                className="reviewsSwiper"
+                className="newBuildingSwiper"
             >
                 {elements}
             </Swiper>
