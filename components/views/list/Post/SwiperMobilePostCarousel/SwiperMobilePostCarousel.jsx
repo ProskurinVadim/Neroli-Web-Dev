99f7da2cefc895e402b195c3a1e773bb59e2ef60
ProskurinVadim <proskurin.vadim@gmail.com>
2023-08-02 00:14:48 +0100

    add working carousel for mobile

--- components/views/list/Post/SwiperMobilePostCarousel/SwiperMobilePostCarousel.jsx
+++ components/views/list/Post/SwiperMobilePostCarousel/SwiperMobilePostCarousel.jsx
@@ -24,18 +24,29 @@ import { Thumbs } from 'swiper/modules';
 
 export default function SwiperMobilePostCarousel({data}) {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
+    const swiperRef = useRef(null);
+    const additionalSwiperRef = useRef(null);
+
+    const onPaginationClick = (e) => swiperRef.current.swiper.slideTo(e.activeIndex);
 
     const mainElements = data.map(item => <SwiperSlide key={nanoid()}><div className={styles.main_image_container}><Image fill={true} src={item.src} alt="street view image" className={styles.main_image} /></div></SwiperSlide>);
 
-    const subElements = data.map(item => <SwiperSlide key={nanoid()}><div className={styles.sub_image_container}><Image fill={true} src={item.src} alt="street view image" className={styles.sub_image} /></div></SwiperSlide>)
+    const subElements = data.map(item => <SwiperSlide key={nanoid()}><div className={styles.sub_image_container}><Image fill={true} src={item.src} alt="street view image" className={styles.sub_image} /></div></SwiperSlide>);
+
+
+    const onUpdate = (e) => additionalSwiperRef.current.swiper.slideTo(e.activeIndex);
+    
+
     return (
         <>
             <Swiper
                 slidesPerView={1}
+                ref={swiperRef}
                 // spaceBetween={40}
                 // centeredSlides={true}
                 // navigation={true}
                 thumbs={{ swiper: thumbsSwiper }}
+                onSlideChange={onUpdate}
                 // navigation={true}
                 // pagination={{
                 //     clickable: true,
@@ -57,9 +68,12 @@ export default function SwiperMobilePostCarousel({data}) {
             </Swiper>
             <div className={styles.subSlider}>
                 <Swiper
+                    ref={additionalSwiperRef}
                     onSwiper={setThumbsSwiper}
                     spaceBetween={15}
+                    onPaginationUpdate={onPaginationClick}
                     navigation={true}
+                        
                     pagination={{
                         clickable: true,
                     }}
