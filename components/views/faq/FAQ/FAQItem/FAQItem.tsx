4fb93afe9d1551c57ac0816d6f9a821b04b2d205
unknown <bogdan.lyamzin.d@gmail.com>
2023-08-18 21:29:09 +0300

    fix

--- components/views/faq/FAQ/FAQItem/FAQItem.tsx
+++ components/views/faq/FAQ/FAQItem/FAQItem.tsx
@@ -12,7 +12,7 @@ const FAQItem: React.FC<IItem> = ({ active, Question, Answer, setActive }) => {
     return (
         <li onClick={setActive} className={styles.faq_list_item}>
             <div className={styles.faq_list_item_content}>
-                <h3 className={"medium_text medium_text__bold"}>{Question}</h3>
+                <h3 className={`medium_text medium_text__bold ${styles.question}`}>{Question}</h3>
                 <span className={styles.faq_list_item_icon}>{active ? <Minus /> : <Plus />}</span>
             </div>
             {active && <p className={`medium_text ${styles.medium_text}`}>{Answer}</p>}
