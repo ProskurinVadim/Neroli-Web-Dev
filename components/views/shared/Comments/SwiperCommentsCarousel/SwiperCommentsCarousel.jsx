dea9d66f30a8474acfe78ae75527e927e1bf013f
unknown <bogdan.lyamzin.d@gmail.com>
2023-11-01 19:52:38 +0200

    add position to comments slider

--- components/views/shared/Comments/SwiperCommentsCarousel/SwiperCommentsCarousel.jsx
+++ components/views/shared/Comments/SwiperCommentsCarousel/SwiperCommentsCarousel.jsx
@@ -1,9 +1,13 @@
-import { useRef, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {nanoid} from "nanoid";
 import Person, { personStyles } from "../../../../shared/Person/Person";
 import styles from "../comments-content.module.scss";
+
+import containerStyles from "../../../../../hoc/Container/Container.module.scss";
+
+import useWidth from "@/hooks/useWidth";
 
 // Import Swiper styles
 import 'swiper/css';
@@ -18,6 +22,19 @@ import { Navigation } from 'swiper/modules';
 
 export default function SwiperCommentsCarousel({data}) {
 
+    const offsetValue = useRef(0);
+
+    useEffect(()=> {
+        // console.log(window.innerWidth)
+        const containerWidth = document.querySelector(`div.${containerStyles.container}`).offsetWidth;
+        const slideWidth = document.querySelector(`div.${personStyles.person_comments}`).offsetWidth;
+        const offset = (window.innerWidth - containerWidth) / 2 + (containerWidth / 2 - slideWidth - 27);
+        offsetValue.current = offset;
+        console.log(offset)
+    }, [])
+
+    const width = useWidth();
+
     const elements = data.map(item => <SwiperSlide className={styles.swiper_slide} key={nanoid()}><Person className={personStyles.person_comments}  {...item} /></SwiperSlide>)
     return (
         <>
@@ -28,11 +45,25 @@ export default function SwiperCommentsCarousel({data}) {
                 // navigation={true}
                 _slideClass="sas"
                 _slideClasses="sss"
-           
+                slidesOffsetBefore={offsetValue.current}
+                slidesOffsetAfter={offsetValue.current}
+                onSetTranslate={(swiper, translate) => {
+                    // console.log("onSetTranslate", firstRender.current);
+                    // const swiperWrapper = document.querySelector(".reviewsSwiper .swiper-wrapper");
+                    // if(firstRender.current && translate === 0) {
+                    //     swiperWrapper.style.transform = "translate3d(200px, 0px, 0px)";
+                    // }
+                    // if(firstRender.current && translate !== 0) {
+                    //     firstRender.current = false;
+                    // }
+
+                    // console.log(swiperWrapper.style.transitionDuration);
+                    // console.log(translate);
+                }}
                 pagination={{
                     clickable: true,
                 }}
-                centeredSlides={true}
+                // centeredSlides={true}
                 breakpoints={{
                     440: {
                         slidesPerView: 1,
