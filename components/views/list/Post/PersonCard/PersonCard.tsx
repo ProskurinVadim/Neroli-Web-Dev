0dad458a92c7e7e4ea0c56d80486f15080cb15e0
unknown <bogdan.lyamzin.d@gmail.com>
2023-12-18 16:50:33 +0200

    add preloader

--- components/views/list/Post/PersonCard/PersonCard.tsx
+++ components/views/list/Post/PersonCard/PersonCard.tsx
@@ -1,10 +1,13 @@
+import {useState, useEffect} from "react";
 import Avatar from "../../../../shared/Person/Avatar";
 import Link from "next/link";
+import Image from 'next/image';
 import Button, { buttonStyles } from "../../../../common/Button/Button";
 import styles from "../Post.module.scss";
 import { IAvatar } from "../../../../../types";
 import { Key, Calendar, Eye, WhatsappUnfill, Phone } from "../../../../icons"
 import PersonCardItem from "./PersonCardItem";
+import icon from "./preloader.svg";
 
 const getData = (onClick: any) => [
     { icon: <Eye />, text: "Arrange a viewing, 24/7"},
@@ -16,7 +19,22 @@ const getData = (onClick: any) => [
 
 const PersonCard: React.FC<IAvatar> = ({ image,  name, job, id, onClick, whatsapp, phone }) => {
     // const onClick2 = () => {}
-    const data = getData(onClick)
+    const [showPreloader, setShowPreloader] = useState(false);
+    const data = getData(onClick);
+
+    useEffect(()=> {
+        if(showPreloader) {
+            setTimeout(()=> setShowPreloader(false), 15000)
+        }
+    }, [showPreloader])
+
+    const togglePreloader = (e)=> {
+        // if(e) {
+        //     e.preventDefault();
+        // }
+        setShowPreloader(prevState => !prevState);
+    };
+
     return (
         <div className={styles.person_card}>
             <Avatar image={image} name={name} job={job} />
@@ -28,7 +46,17 @@ const PersonCard: React.FC<IAvatar> = ({ image,  name, job, id, onClick, whatsap
                 data.map((elem,i) => <PersonCardItem {...elem} key={`person-item-${i}`}/>)
             }
             <Button onClick={onClick} text="Book a Viewing" className={styles.button} />
-            <a href={`https://pdf-presentation.onrender.com/api/presentation/${id}`}><Button onClick={()=>{}} className={`${buttonStyles.button__transparent} ${styles.button}`} text="Download presentation" /></a>
+            {/*<a href={`https://pdf-presentation.onrender.com/api/presentation/${id}`}><Button onClick={()=>{}} className={`${buttonStyles.button__transparent} ${styles.button}`} text="Download presentation" /></a>*/}
+            <a onClick={togglePreloader} href={`https://pdf-presentation.onrender.com/api/presentation/${id}`} className={`${styles.download} ${styles.button} ${showPreloader ? styles.load : ""}`}>
+                {showPreloader ? <Image
+                    priority
+                    width={40}
+                    height={40}
+                    src={icon}
+                    alt="preloader"
+                /> : "Download presentation"}
+            </a>
+            {showPreloader && <p className={styles.preloaderText}>It can take up to 1 minute to generate a presentation.</p>}
             {/*<a href={`http://localhost:4000/api/presentation/${id}`}><Button onClick={()=>{}} className={`${buttonStyles.button__transparent} ${styles.button}`} text="Download presentation" /></a>*/}
         </div>
     )
