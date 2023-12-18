c3912f85909dd36d288a3e4e57e2fc8aaf53bfe6
ProskurinVadim <proskurin.vadim@gmail.com>
2023-12-18 21:53:24 +0200

    add search

--- components/views/home/Main/Main.tsx
+++ components/views/home/Main/Main.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import { Button } from "../../../common";
 import Toggler, { togglerStyles} from "../../../common/Toggler/Toggler";
@@ -8,40 +8,109 @@ import Link from 'next/link';
 import Container from "../../../../hoc/Container";
 import { IFormFields } from "../../../../types";
 import styles from "./Main.module.scss";
-import { defaultData, getFormData } from "./getData";
+import Input,{ inputStyles } from "../../../common/Input/Input";
+import { getAppartments } from "@/utils/fetch";
+import { defaultData, getFormData, getModalFormData } from "./getData";
+
 interface IForm {
     building: string,
     rest: string,
 }
 
 
+interface IModalForm {
+    property_type: string,
+    price_min: string,
+    price_max: string,
+    beds: string,
+}
+
+interface IList {
+    id: number,
+    title: string,
+}
+
+
 export default function Home() {
+    const ref = useRef<any>(null);
     const [active, setActive] = useState<string>("Residential");
     const [value, setValue] = useState<IForm>({ ...defaultData });
-    const fields: any = getFormData();
     const { push } = useRouter();
+    const [modalValue, setmodalValue] = useState<any>({property_type: "", price_min: "", price_max: "", beds:""});
+    const [list,setList] = useState<IList[]>([])
+    const [open,setOpen] = useState<boolean>(false);
+    const [listOpen,setListOpen] = useState<boolean>(false);
+    const [selectOpen,setSelectOpen] = useState<boolean | string>(false);
+    const fields: any = getFormData(()=>setOpen(true), ()=> setListOpen(true), ()=> setListOpen(false));
+    const modlFields: any = getModalFormData(setSelectOpen,selectOpen);
+    
+    useEffect(() => {
+        const handleOutSideClick = (event: any) => {
+            if (!ref.current?.contains(event.target)) {
+                setOpen(()=> false);
+            }
+        };
+
+        window.addEventListener("mousedown", handleOutSideClick);
+
+        return () => {
+            window.removeEventListener("mousedown", handleOutSideClick);
+        };
+    }, [ref]);
+
+    useEffect(()=> {
+        if(value.building) {
+            const getData = setTimeout(() => {
+                getAppartments({type:active,...modalValue, building: value.building}).then(({data})=> {
+                    const list = data.map((elem:any) => ({id: elem.id, title : elem.attributes.Title}));
+                    setList(list);
+                });
+            }, 2000);
+          return () => clearTimeout(getData)
+
+        }
+    },[value.building])
 
     const handelSetActive = (active: string) => setActive(_ => active);
     const handelSubmit = () => {
-        const { building, rest } = value;
-        let query = `?type=${active}`
+        const { building } = value;
+        const {property_type, price_min, price_max, beds}  = modalValue;
+        let query = `?type=${active}`;
         if (building) {
-            query += `&building=${building}`
+            query += `&building=${building}`;
         }
-        rest.split(" ").map((elem: string) => {
-            if (isNaN(+elem) && !query.includes("&type") && elem) {
-                query += `&property_type=${elem}`
-            }
-            else if (+elem < 10 && !query.includes("&beds") && elem) {
-                query += `&beds=${elem}`
-            }
-            if (+elem >= 10 && !query.includes("&price_min")) {
-                query += `&price_min=${elem}`
-            }
-            push(`/list${query}`)
-        })
+        if (isNaN(+property_type) && property_type) {
+            query += `&property_type=${property_type}`;
+        }
+        if (isNaN(+price_min)  && price_min) {
+            query += `&price_min=${price_min}`;
+        }
+        if (isNaN(+price_max) && price_max) {
+            query += `&price_max=${price_max}`;
+        }
+        if (beds)    {
+            query += `&beds=${beds}`;
+        }
+        push(`/list${query}`);
     };
     const onClick = () => false;
+
+    const handelChange = (key: string) => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setmodalValue((prev_value: any) => {
+            const newValue = { ...prev_value };
+            newValue[key] = e.target.value;
+            const value = Object.values(newValue).filter(elem => elem).join(",");
+            setValue((prev:any) => {
+                return {
+                    ...prev,
+                    rest: value
+                }
+            })
+            return { ...newValue }
+        })
+
+    };
+
     return (
         <section className={`${styles.main_page}`}>
             <Container>
@@ -50,7 +119,24 @@ export default function Home() {
                 <div className={`${styles.toggler_container}`} >
                     <Toggler array={["Residential", "Commercial", "Off-plan"]} active={active} setActive={handelSetActive} className={`${styles.toggler} ${togglerStyles.toggler__fill}`} />
                 </div>
-                <Form fields={fields} onSubmit={handelSubmit} value={value} setValue={setValue} className={`${formStyles.form__search} ${styles.search}`} buttonText="Search"/>
+                <div className={styles.main_page_search_container}>
+                    <Form className={`${formStyles.form__search} ${styles.search}`} fields={fields} onSubmit={handelSubmit} value={value} setValue={setValue}  buttonText="Search">
+                    {(listOpen && list.length) || true && <ul className={styles.main_page_search_modal}>
+                        {list.map((elem: any,i) => <li key={"list-item-"+i} className={styles.main_page_search_modal_item}>
+                            <Link href={"/list/"+elem.id}>{elem.title}</Link>
+                        </li>)}
+                    </ul>}
+                    {open && 
+                    <ul ref={ref} className={styles.main_page_search_select_modal}>
+                        {
+                            modlFields.map((elem: any) => <li>
+                                {elem.render(modalValue[elem.key],handelChange(elem.key))}
+                            </li>)
+                        }
+                    </ul>
+                    }
+                    </Form>
+                </div>
                 <p className={`medium_text ${styles.medium_text} medium_text__light`}>Prices are always changing, find out the value of your property today</p>
                 <div className={styles.button_wrapper}>
                     <Link href="/contact-us"><Button text="Contact us" onClick={onClick} /></Link>
--- components/views/home/Main/getData.tsx
+++ components/views/home/Main/getData.tsx
@@ -1,14 +1,16 @@
 import Input,{ inputStyles } from "../../../common/Input/Input";
-export const getFormData = () => [
+import Select from "../../../common/Select/Select";
+
+export const getFormData = (onFocus:any,onBuildingFocus:any,onBuildingBlur: any) => [
     {
         key: "building",
         render: (value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void) =>
-            (<Input value={value} onChange={onChange} placeholder="Community or building" className={`${inputStyles.input_inline} ${inputStyles.input_inline__rounded}`} containerClassName={inputStyles.input_inline_container} />)
+            (<Input value={value} onFocus={onBuildingFocus} onBlur={onBuildingBlur} onChange={onChange} placeholder="Community or building" className={`${inputStyles.input_inline} ${inputStyles.input_inline__rounded}`} containerClassName={inputStyles.input_inline_container} />)
     },
     {
         key: "rest",
         render: (value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void) =>
-            (<Input value={value} onChange={onChange} placeholder="Price, Type and Bedrooms" className={`${inputStyles.input_inline}`} containerClassName={inputStyles.input_inline_container} />)
+            (<Input value={value} onFocus={onFocus} onChange={onChange} placeholder="Price, Type and Bedrooms" className={`${inputStyles.input_inline}`} containerClassName={inputStyles.input_inline_container} />)
     }
 ]
 
@@ -16,3 +18,110 @@ export const defaultData = {
     building: "",
     rest: "",
 }
+
+
+const bedroomsOptions = [
+    { value: "", name: "All" },
+    { value: "Studio", name: "Studio" },
+    { value: "Bedrooms 1", name: "1 Bedroom" },
+    { value: "Bedrooms 2", name: "2 Bedroom" },
+    { value: "Bedrooms 3", name: "3 Bedroom" },
+    { value: "Bedrooms 4", name: "4 Bedroom" },
+    { value: "Bedrooms 5", name: "5 Bedroom" },
+    { value: "Bedrooms 6", name: "6 Bedroom" },
+    { value: "Bedrooms 7+", name: "7 + Bedroom" },
+];;
+
+const price = [
+    { value: "", name: "All" },
+    { value: "500000", name: "AED 500, 000" },
+    { value: "750000", name: "AED 750, 000" },
+    { value: "1000000", name: "AED 1, 000, 000" },
+    { value: "2000000", name: "AED 2, 000, 000" },
+    { value: "3000000", name: "AED 3, 000, 000" },
+    { value: "4000000", name: "AED 4, 000, 000" },
+    { value: "5000000", name: "AED 5, 000, 000" },
+    { value: "6000000", name: "AED 6, 000, 000" },
+    { value: "7000000", name: "AED 7, 000, 000" },
+    { value: "8000000", name: "AED 8, 000, 000" },
+    { value: "9000000", name: "AED 9, 000, 000" },
+    { value: "10000000", name: "AED 10, 000, 000" },
+    { value: "20000000", name: "AED 20, 000, 000" },
+    { value: "30000000", name: "AED 30, 000, 000" },
+    { value: "40000000", name: "AED 40, 000, 000" },
+    { value: "50000000", name: "AED 50, 000, 000" },
+    { value: "60000000", name: "AED 60, 000, 000" },
+    { value: "70000000", name: "AED 70, 000, 000" },
+    { value: "80000000", name: "AED 80, 000, 000" },
+];
+
+const typeOptions = [
+    { value: "", name: "All" },
+    { value: "Apartment", name: "Apartment" },
+    { value: "Villa", name: "Villa" },
+    { value: "Townhouse", name: "Townhouse" },
+
+    { value: "Penthouse", name: "Penthouse" },
+    { value: "Duplex", name: "Duplex" },
+    { value: "Plot", name: "Plot" },
+    { value: "Land", name: "Land" }
+];
+
+export const getModalFormData = (setOpen: (open: boolean | string) => void,open:string | boolean) => [
+    {
+        key: "property_type",
+        render: (value: string, onChange: (newValue: string) => void) =>
+            (<Select value={value} onChange={onChange} label="type" open={open === "type"} setOpen={setOpen} options={typeOptions} defaultValue="Property Type"/>)
+    },
+    {
+        key: "beds",
+        render: (value: string, onChange: (newValue: string) => void) =>
+            (<Select value={value} onChange={onChange} label="beds" open={open === "beds"} setOpen={setOpen} options={bedroomsOptions} defaultValue="Beds"/>)
+    },
+    {
+        key: "price_min",
+        render: (value: string, onChange: (newValue: string) => void) =>
+            (<Select value={value} onChange={onChange} label="min" open={open === "min"} setOpen={setOpen} options={price} defaultValue="Price Min" />)
+    },
+    {
+        key: "price_max",
+        render: (value: string, onChange: (newValue: string) => void) =>
+            (<Select value={value} onChange={onChange} label="max" open={open === "max"} setOpen={setOpen} options={price} defaultValue="Price Max" />)
+    },
+];
+
+
+type queryType = string | null;
+
+const getDefaultBeds = (beds: queryType) => {
+    if (beds === null) return ""
+    else if (+beds === 0) return "Studio"
+    if (+beds <= 6) return `Bedrooms ${beds}`
+    else return "Bedrooms 7+"
+
+}
+const getDefaultType = (type: queryType) => {
+    let res = "";
+    if (type) {
+        const match = typeOptions.filter(
+            elem => elem.value.toLowerCase() === type.toLowerCase()
+        );
+        res = match[0] ? match[0].value : ""
+    }
+
+    return res
+}
+const getDefaultPrice = (price: queryType) => {
+    if (price) return typeOptions.filter(
+        elem => elem.value <= price
+    )[0].value;
+
+    return ""
+}
+export const getDefaultData = (property_type: queryType, building: queryType, price_min: queryType, beds: queryType) => ({
+    building: building || "",
+    property_type: getDefaultType(property_type),
+    beds: getDefaultBeds(beds),
+    price_min: getDefaultPrice(price_min),
+    price_max: "",
+});
