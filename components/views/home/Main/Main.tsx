e293973919ed5d5e03da6470cfc8c7680d3afcd3
unknown <bogdan.lyamzin.d@gmail.com>
2024-02-01 11:13:51 +0200

    add micro change

--- components/views/home/Main/Main.tsx
+++ components/views/home/Main/Main.tsx
@@ -132,7 +132,7 @@ export default function Home() {
     return (
         <section className={`${styles.main_page}`}>
             <Container>
-                <h2 className={`small_section_header small_section_header__light ${styles.main_sub_header}`}>Welcome to Form Estate</h2>
+                <h2 className={`small_section_header small_section_header__light ${styles.main_sub_header}`}>Welcome to Neroli Properties</h2>
                 <h1 className={styles.main_header}>Discover Architectural Elegance:<br /> Where Exquisite Homes Meet Unparalleled Expertise</h1>
                 <div className={`${styles.toggler_container}`} >
                     <Toggler array={["Residential", "Commercial", "Off-plan"]} active={active} setActive={handelSetActive} className={`${styles.toggler} ${togglerStyles.toggler__fill}`} />
